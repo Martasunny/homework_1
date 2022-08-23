c5cce8ef74009075b74aa5a6e97d6a6b8b2cd2e6
Marta <[martasunnyy@gmail.com]>
2022-08-23 21:01:30 +0300

    решила задачу 6

---
+++ задача_4/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Enter first number");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter third number");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b && a > c)
+{
+    Console.WriteLine("max number:" + a);
+}
+else if (b>a && b>c)
+{
+    Console.WriteLine("max number:" + b);
+}
+else 
+{
+    Console.WriteLine("max number:" + c);
+}
---
+++ задача_6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Enter number");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("вы ввели четное число");
+}
+else 
+{
+    Console.WriteLine("вы ввели нечетное число");
+}
